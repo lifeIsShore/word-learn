f251f58411224105235b28a8bd6a852ac1f22264
LifeIsShore <ahmtyagmur03@gmail.com>
2026-03-09 13:31:06 +0300

    hot fıx buıld

--- word_learn/android/settings.gradle.kts
+++ word_learn/android/settings.gradle.kts
@@ -20,7 +20,7 @@ pluginManagement {
 plugins {
     id("dev.flutter.flutter-plugin-loader") version "1.0.0"
     id("com.android.application") version "8.7.2" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.25" apply false
+    id("org.jetbrains.kotlin.android") version "2.1.0" apply false
     // NOTE: google-services plugin intentionally omitted until
     // google-services.json is added. Add back with:
     //   id("com.google.gms.google-services") version "4.4.2" apply false
